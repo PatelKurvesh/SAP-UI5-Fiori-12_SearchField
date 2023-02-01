0d5d55e6f549948b6f3ff936a24df561c6010ee9
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-01 21:48:32 +0530

    Updated View

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -21,10 +21,21 @@ sap.ui.define([
 			//oprand 1(in which you want to search) / opretor/ oprand 2(search query)
 			// var oName = new sap.ui.model.Filter("Oprand1",Opretor,"Oprand2");
 			var oName = new sap.ui.model.Filter("name",sap.ui.model.FilterOperator.Contains,queryString);
+			//if you need to search all property than you need to create a master filter.
+			var oColor = new sap.ui.model.Filter("color",sap.ui.model.FilterOperator.Contains,queryString);
+			var oTaste = new sap.ui.model.Filter("taste",sap.ui.model.FilterOperator.Contains,queryString);
+			var oPrice = new sap.ui.model.Filter("price",sap.ui.model.FilterOperator.Contains,queryString);
+			
+			var masterFilter = new sap.ui.model.Filter({
+				filters:[oName,oColor,oTaste,oPrice],
+				and : false
+			});
+			
 			
 			//Step 3) Create a filter array.
 			//List filter function work with array.
-			var filter = [oName];
+			// var filter = [oName];
+			var filter = [masterFilter];     //if you use master filter.
 			
 			var fruitList = this.getView().byId("fruits");
 			fruitList.getBinding("items").filter(filter);
